18ce4f9845544e295b05d283f357fd8677bd66b3
trangngocduythanhh <trangngocduythanhh@example.com>
2025-07-02 21:24:36 +0700

    First commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+    root: "src",
+    publicDir: "../public",
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "src/index.html"),
+                about: path.resolve(__dirname, "src/about.html"),
+                // ...
+            },
+        },
+    },
+    plugins: [ViteEjsPlugin()],
+});
