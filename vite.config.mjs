3f92347e70623f1cd4e572f713afcf9c6dc0961e
trangngocduythanhh <trangngocduythanhh@example.com>
2025-07-02 22:40:08 +0700

    Doi ten vite.config.js thanh vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import { ViteEjsPlugin } from "vite-plugin-ejs";
 import path from "path";
 
 export default defineConfig({
+    base: "/f8-zoom-module-1",
     root: "src",
     publicDir: "../public",
     build: {
